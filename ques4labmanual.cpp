baf8bf4286e803b8c0bafc4c70cf5dc48211fc85
HardikJuneja10 <hardikjuneja10@gmail.com>
2026-01-20 21:45:49 +0530

    Add files via upload

---
+++ ques4labmanual.cpp
@@ -0,0 +1,23 @@
+//Q4 A shopkeeper wants to calculate the total bill amount after applying a 20% discount on the purchase.
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n,u,q;
+
+    cout<<"ENTER THE ITEM NUMBER ";
+    cin>>n;
+
+    
+    cout<<"ENTER THE UNIT PRICE ";
+    cin>>u;
+
+    
+    cout<<"ENTER THE QUANTITY ";
+    cin>>q;
+
+    cout<<"TOTAL BILL IS "<<0.8*q*u;
+
+
+}
